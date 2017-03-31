496f6d5ddc6f06f0760a4cc35975ae9cc72fec88
jasonhilimire <jasonhilimire@mac.com>
2017-03-30 21:26:05 -0600

    Initial Commit

---
+++ VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by Jason Hilimire on 3/30/17.
+//  Copyright © 2017 Peanut Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
